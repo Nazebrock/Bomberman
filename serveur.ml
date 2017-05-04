73d504263bcf5fe8a75e44e4d32d1a79b4635239
flobre <florian.breton1@u-psud.fr>
2017-05-04 11:21:29 +0200

    reseau etabli

--- serveur.ml
+++ serveur.ml
@@ -4,7 +4,7 @@ let boucle_Ecoute() =
  demarrer_le_serveur 7885;
  let c0 = attendre_connexion_client () in
  envoyer_message_au_client "vous êtes connecté au serveur" c0;
- print_string ""
+ print_string "message envoyé"
 ;;
 
 boucle_Ecoute ();;
--- simuclient.ml
+++ simuclient.ml
@@ -1,7 +1,14 @@
 open Reseau;;
 
 let essai_Connexion() =
- connexion_au_serveur "" 7885
+ connexion_au_serveur "" 7885;
+ let msg =let m=recevoir_un_message_du_serveur () in match m with
+	|string -> print_string m
+ 	|_->()
+ in msg
+
 ;;
+
+essai_Connexion () ;;
 
 
