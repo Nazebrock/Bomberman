3309a2621e7d2c40e78212fa94000e63db3c03c0
jerome.lorrain <jeromev21337@gmail.com>
2017-05-04 18:06:47 +0200

    Ajout de la gestion des bombe avec:     - gestion des bloc cassable     - gestion de la mort d'un joueur

--- client.ml
+++ client.ml
@@ -10,15 +10,25 @@ let nbr_lig = ref 0;;
 (* Initialise un joueur *)
 let player = ref {x=100;y=100;couleur=Bleu;dir=Nord;etat=Vivant;pas=Some 0};;
 let bloc = Bloc {blk_i = 5; blk_j = 5; blk_forme = Cassable Intact};;
+
 (** Plateau
  *  0 = Sol
- *  1 = Cassable
- *  2 = incassable
+ *  1 = Bombe
+ *  2 = flamme
+ *  10 = Cassable Brise 
+ *  11 = Cassable Intact
+ *  12 = incassable
  *)
-let board = Array.create_matrix 10 10 0;;
-board.(5).(8) <- 1;;
-board.(0).(0) <- 2;;
-board.(9).(9) <- 1;;
+let board = Array.make_matrix 10 10 0;;
+board.(5).(8) <- 11;;
+board.(0).(0) <- 12;;
+board.(9).(9) <- 11;;
+
+(** Stock les bombes active *)
+let active_bombe = ref [];;
+(** Stock les bombes en train d'exploser *)
+let explosing_bombe = ref [];;
+
 let initBoard b =
     Printf.printf "INIT BOARD\n";
     nbr_col := Array.length b.(0);
@@ -26,13 +36,12 @@ let initBoard b =
     Ig.init width heigth !nbr_col !nbr_lig;
     for i = 0 to !nbr_lig - 1  do
         for j = 0 to !nbr_col - 1 do
-            if b.(i).(j) = 1 then
+            if b.(i).(j) = 11 then
                 Ig.affiche_sprite (Bloc {blk_i = i; blk_j = j; blk_forme = Cassable Intact})
-            else if b.(i).(j) = 2 then
+            else if b.(i).(j) = 12 then
                 Ig.affiche_sprite (Bloc {blk_i = i; blk_j = j; blk_forme = Incassable})
         done;
     done;
-    Ig.affiche_sprite (Bomberman !player);
     Ig.affiche (); 
 ;;
 
@@ -47,15 +56,15 @@ let change_dir_player p = function
 
 (** Verifie que le point x y est sur une case libre *)
 let check_point x y =
-    Printf.printf "Check %d %d = Case %d %d\n" x y (x / width) (y / heigth);
-    if board.(x / width).(y / heigth) != 0 then
+    if board.(x / width).(y / heigth) > 9 then
         false
     else
         true
 ;;
 
+(** Modifie la position du joueur en fonction de sa direction
+ *  Prend en compte les colisions avec les bloc et les bord du plateau *)
 let move_player () =
-    Ig.efface_sprite (Bomberman !player);
     let new_p = match !player.dir with
         | Nord ->   
                 let y = !player.y + (p_heigth/2) in let x = !player.x in
@@ -99,7 +108,6 @@ let move_player () =
                         {!player with x = !player.x + pas}
     in
     player := new_p;
-    Ig.affiche_sprite (Bomberman !player);
 ;;
 
 (** Incremente le champs pas du joueur *)
@@ -114,19 +122,122 @@ let incr_pas () =
     player := {!player with pas = a}
 ;;
 
+let add_bombe () =
+    let x = !player.x/width in let y = !player.y/heigth in
+    if board.(x).(y) = 0 then begin
+        board.(x).(y) <- 1;
+        let b = {b_i = x; b_j = y; b_duree = 16} in
+        active_bombe := !active_bombe@[b];
+    end
+;;
+
+let check_player x y =
+    if (!player.x/width) = x && (!player.y/heigth) = y then
+        player := {!player with etat = Grille}
+;;
+
+let add_flamme x y =
+    let dir = [|[|1;0|] ; [|-1;0|] ; [|0;1|] ; [|0;-1|]|] in
+    let forme = [|FDroite;FGauche;FHaut;FBas|] in
+    board.(x).(y) <- 2;
+    Ig.affiche_sprite (Flamme {f_i = x; f_j = y; f_forme = FCroix});
+    for i = 0 to 3 do
+        let b_i = x+dir.(i).(0) in let b_j = y+dir.(i).(1) in
+        try
+            let case = board.(b_i).(b_j) in
+            match case with
+                | 11 -> board.(b_i).(b_j) <- 10;
+                        Ig.affiche_sprite (Bloc {blk_i = b_i; blk_j = b_j; blk_forme = Cassable Brise});
+                | 12 -> ()
+                | _ -> board.(b_i).(b_j) <- 2;
+                        Ig.affiche_sprite (Flamme {f_i = b_i; f_j = b_j; f_forme = forme.(i)});
+            check_player x y;
+        with Invalid_argument  s -> ();
+    done;
+;;
+
+let remove_flamme x y =
+    let dir = [|[|1;0|] ; [|-1;0|] ; [|0;1|] ; [|0;-1|]|] in
+    board.(x).(y) <- 0;
+    Ig.efface_sprite (Flamme {f_i = x; f_j = y; f_forme = FCroix});
+    for i = 0 to 3 do
+        let b_i = x+dir.(i).(0) in let b_j = y+dir.(i).(1) in
+        try
+            let case = board.(b_i).(b_j) in
+            if case = 2 then begin
+                board.(b_i).(b_j) <- 0;
+                Ig.efface_sprite (Flamme {f_i = b_i; f_j = b_j; f_forme = FCroix});
+            end
+        with Invalid_argument  s -> ();
+    done;
+;;
+
+let explode_bombe () =
+    explosing_bombe := List.fold_left
+        (fun l a ->
+            let x = a.b_i in let y = a.b_j in
+            if a.b_duree = -1 then begin
+                remove_flamme x y;
+                l
+            end
+            else begin
+                add_flamme x y;
+                {a with b_duree = -1}::l
+            end
+        ) [] !explosing_bombe;
+;;
+
+let refresh_bombe () = 
+    active_bombe := List.fold_left 
+        (fun l a ->
+            if a.b_duree = 0 then begin
+                explosing_bombe := !explosing_bombe@[{a with b_duree = -2}];
+                l
+            end
+            else begin
+                Ig.efface_sprite (Bombe a);
+                let b = {a with b_duree = a.b_duree-1} in
+                Ig.affiche_sprite (Bombe b);
+                b::l
+            end
+        ) [] !active_bombe;
+;;
+
+let refresh_player () =
+    match !player.etat with
+        | Vivant -> Ig.efface_sprite (Bomberman !player);
+                    move_player ();
+                    Ig.affiche_sprite (Bomberman !player);
+        | Grille -> Ig.efface_sprite (Bomberman !player);
+                    Ig.affiche_sprite (Bomberman !player);
+                    player := {!player with etat = Mort};
+        | Mort ->   Ig.efface_sprite (Bomberman !player);
+                    Ig.affiche_sprite (Bomberman !player);
+;;
+
 let gameLoop () =
     let fin = ref false in
+    let bombe = ref false in
     while not !fin do
         Unix.sleepf 0.03;
         if Ig.touche_pressee () then
             begin
-                player := change_dir_player !player (Ig.lecture_touche ());
+                let c = Ig.lecture_touche () in
+                if c = 'b' then
+                    add_bombe ()
+                else
+                    player := change_dir_player !player c
             end
         else
             player := !player;
         incr_pas ();
-        move_player ();
+        if !bombe then 
+            refresh_bombe ()
+        else
+            explode_bombe ();
+        refresh_player ();
         Ig.affiche ();
+        bombe := not !bombe;
     done;
 ;;
 
