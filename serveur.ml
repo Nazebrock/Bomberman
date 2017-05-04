d37c23737bab342d7b46e795ba25e3f28d636e91
flobre <florian.breton1@u-psud.fr>
2017-05-04 11:56:38 +0200

    avancement du reseau

--- serveur.ml
+++ serveur.ml
@@ -1,11 +1,18 @@
 open Reseau;;
 
-let boucle_Ecoute() =
+let ecoute() =
  demarrer_le_serveur 7885;
- let c0 = attendre_connexion_client () in
- envoyer_message_au_client "vous êtes connecté au serveur" c0;
- print_string "message envoyé"
+ let c0 = attendre_connexion_client () 
+	in
+ 	envoyer_message_au_client "vous êtes connecté au serveur" c0;
+ let reqlist = lire_requetes_clients [c0]
+ 	in
+	match reqlist with
+	|[] -> print_string "aucun msg dans boite aux lettres"
+	|x::s -> match x with |MessageClient(c,m)-> print_string m
+			      |DeconnexionClient(c) -> print_string "deco"
 ;;
 
-boucle_Ecoute ();;
+ecoute ()
+
 
--- simuclient.ml
+++ simuclient.ml
@@ -1,14 +1,16 @@
 open Reseau;;
 
 let essai_Connexion() =
- connexion_au_serveur "" 7885;
- let msg =let m=recevoir_un_message_du_serveur () in match m with
+connexion_au_serveur "" 7885;
+ let msg =
+	let m=recevoir_un_message_du_serveur () in 
+	match m with
 	|string -> print_string m
  	|_->()
- in msg
-
-;;
+ in msg;
+envoyer_message_au_serveur "salut je suis client";;
 
 essai_Connexion () ;;
+
 
 
