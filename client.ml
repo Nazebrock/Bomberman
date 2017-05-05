53b9b4506b558cbd01c73bc013f5148c25ab2e0d
jerome.lorrain <jeromev21337@gmail.com>
2017-05-05 12:18:36 +0200

    WIP: Creation de map a partir d'une Sting

--- client.ml
+++ client.ml
@@ -1,17 +1,16 @@
 open Gtypes;;
 open Player;;
 open Bombe;;
-
-let pas = 5;;
+open String;;
 let width = 92;;
 let heigth = 72;;
 let p_width = 48;;
 let p_heigth = 70;;
 let nbr_col = ref 0;;
 let nbr_lig = ref 0;;
+
 (* Initialise un joueur *)
-let player = ref {x=100;y=100;couleur=Bleu;dir=Nord;etat=Vivant;pas=Some 0};;
-let bloc = Bloc {blk_i = 5; blk_j = 5; blk_forme = Cassable Intact};;
+let player = ref {x=116;y=107;couleur=Bleu;dir=Nord;etat=Vivant;pas=Some 0};;
 
 (** Plateau
  *  0 = Sol
@@ -21,20 +20,35 @@ let bloc = Bloc {blk_i = 5; blk_j = 5; blk_forme = Cassable Intact};;
  *  11 = Cassable Intact
  *  12 = incassable
  *)
-let board = Array.make_matrix 10 10 0;;
-board.(5).(8) <- 11;;
-board.(0).(0) <- 12;;
-board.(9).(9) <- 11;;
+let board = ref [||];;
 
 (** Stock les bombes active *)
 let active_bombe = ref [];;
 (** Stock les bombes en train d'exploser *)
 let explosing_bombe = ref [];;
 
+let read_map map = 
+    let b = ref [] in let ligne = ref [] in
+    Printf.printf "Board %d\n" (String.length map);
+    String.iter (function
+        | '=' -> ligne := !ligne@[12]
+        | 'x' -> ligne := !ligne@[11]
+        | '\n'-> b := !b@[Array.of_list !ligne]; ligne := []
+        | _ -> ligne := !ligne@[0]
+    ) map;
+    board := Array.of_list !b;
+;;
+
 let initBoard b =
     Printf.printf "INIT BOARD\n";
+    Array.iter (fun a -> Array.iter (function
+        |12 -> Printf.printf "="
+        |11 -> Printf.printf "x"
+        | 0 -> Printf.printf " "
+    ) a; Printf.printf "\n") b;
     nbr_col := Array.length b.(0);
     nbr_lig := Array.length b;
+    Printf.printf "Board %d %d\n" !nbr_col !nbr_lig;
     Ig.init width heigth !nbr_col !nbr_lig;
     for i = 0 to !nbr_lig - 1  do
         for j = 0 to !nbr_col - 1 do
@@ -48,10 +62,10 @@ let initBoard b =
 ;;
 
 let refresh_player () =
-    check_player player board;
+    check_player player !board;
     match !player.etat with
         | Vivant -> Ig.efface_sprite (Bomberman !player);
-                    player := move_player player board;
+                    player := move_player player !board;
                     Ig.affiche_sprite (Bomberman !player);
         | Grille -> Ig.efface_sprite (Bomberman !player);
                     Ig.affiche_sprite (Bomberman !player);
@@ -69,7 +83,7 @@ let gameLoop () =
             begin
                 let c = Ig.lecture_touche () in
                 if c = 'b' then
-                    add_bombe (!player.x/width) (!player.y/heigth) board active_bombe
+                    add_bombe (!player.x/width) (!player.y/heigth) !board active_bombe
                 else
                     change_dir_player player c
             end
@@ -78,14 +92,16 @@ let gameLoop () =
         if !bombe then 
             refresh_bombe active_bombe explosing_bombe
         else
-            explode_bombe explosing_bombe board;
+            explode_bombe explosing_bombe !board;
         refresh_player ();
         Ig.affiche ();
         bombe := not !bombe;
     done;
 ;;
 
+let map = "==========\n=    x   =\n=  = ==  =\n= x  =   =\n= =  =   =\n= x  =   =\n= =  x = =\n=  =  =  =\n==========\n";;
 
-initBoard board;;
+read_map map;;
+initBoard !board;;
 Printf.printf "Game RUNNING\n";
 gameLoop () ;;
